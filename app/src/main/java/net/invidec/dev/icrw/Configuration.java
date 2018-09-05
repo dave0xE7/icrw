8c712c57ef71773c9858a284305069e7cd495be2
David Wieser <david.wieser.202@gmail.com>
2018-09-05 15:57:33 +0200

    check

--- app/src/main/java/net/invidec/dev/icrw/Configuration.java
+++ app/src/main/java/net/invidec/dev/icrw/Configuration.java
@@ -3,7 +3,7 @@ package net.invidec.dev.icrw;
 public class Configuration {
     private static final String app_host = "195.201.178.220";
     private static final String domain_path = "http://" + app_host + "/";
-    private static final String app_auth = domain_path + "icrw-server/api2.php";
+    private static final String app_auth = domain_path + "icrw-server/api.php";
     private static final String app_pricecharts = domain_path + "icrw-server/pricecharts.json";
 
     public static String apiUrl_EURBTC = "https://beatcoin.pl/public/graphs/EURBTC/1day.json";
